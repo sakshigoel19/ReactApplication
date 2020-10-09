479683d9792d06b99518e3078f96d3b28cb1626d
Sakshi Goel <sakshigoel1901@gmail.com>
2020-10-10 01:40:02 +0530

    Second Assignment

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import {LEADERS} from '../shared/leaders';
 import {PROMOTIONS} from '../shared/promotions';
 import Home from'./HomeComponent';
 import Contact from "./ContactComponent";
+import About from "./AboutComponent";
 import {Switch , Route , Redirect } from 'react-router-dom';
 
 class Main extends Component{
@@ -41,6 +42,12 @@ class Main extends Component{
             comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
       );
     };
+
+    const AboutUs =()=>{
+      return(
+        <About leaders={this.state.leaders}/>
+      );
+    }
       return (
         <div>
         <Header/>
@@ -49,6 +56,7 @@ class Main extends Component{
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes}/>}/>
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path="/contactus" component={Contact}/>
+              <Route path='/aboutus' component={AboutUs}/>
               <Redirect to='/home' />
             </Switch>
         <Footer/>
