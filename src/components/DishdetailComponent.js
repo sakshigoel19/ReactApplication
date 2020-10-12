4a7a85c532714d0338ff99012119560d7f16b0bc
Sakshi Goel <sakshigoel1901@gmail.com>
2020-10-12 13:16:03 +0530

    Third Assignment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,8 +1,87 @@
-import React from 'react';
-import {Card, CardImg,CardImgOverlay,CardText,CardBody,CardTitle,BreadcrumbItem,Breadcrumb} from 'reactstrap'
+import React,{Component} from 'react';
+import {Card, CardImg,CardImgOverlay,CardText,CardBody,CardTitle,BreadcrumbItem,Breadcrumb, Button, Modal, ModalBody,ModalHeader,Row,Col,Label} from 'reactstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Link} from 'react-router-dom';
+import {Control,LocalForm,Errors} from 'react-redux-form';
 
+const required=(val)=>val&&val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+
+class CommentForm extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            isModalOpen : false,
+        }
+        this.toggleModal=this.toggleModal.bind(this);
+        this.handleSubmit=this.handleSubmit.bind(this);
+    }
+
+    toggleModal(){
+        this.setState({
+            isModalOpen:!this.state.isModalOpen
+        });
+    }
+
+    handleSubmit(values) {
+        this.toggleModal();
+        alert("current state"+ JSON.stringify(values));
+    }
+    render(){
+        return(
+        <div>
+        <Button outline onClick={this.toggleModal}><span className="fa fa-pencil fa-md">Submit Comment</span>
+        </Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+        <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+        <ModalBody>
+            <LocalForm onSubmit={(values)=>this.handleSubmit(values)}>
+            <Row className="form-group m-1">
+            <Label htmlFor="rating">Rating</Label>
+            <Control.select model=".rating" name="rating" className="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                    </Control.select>
+
+            </Row>
+            <Row className="form-group m-1">
+                <Label htmlFor="author">Your name</Label>
+                <Control.text model=".author" id="author" name="author" placeholder="Your Name" className="form-control"
+                 validators={{
+                    required, minLength: minLength(3), maxLength: maxLength(15)
+                }}
+                 />
+                 <Errors
+                                    className="text-danger"
+                                    model=".author"
+                                    show="touched"
+                                    messages={{
+                                        required: 'Required',
+                                        minLength: 'Must be greater than 2 characters',
+                                        maxLength: 'Must be 15 characters or less'
+                                    }}
+                                    />
+            </Row>
+            <Row className="form-group m-1">
+                <Label htmlFor="feedback">Comment</Label>
+                <Control.textarea model=".feedback" id="feedback" name="feedback" rows="6" className="form-control"/>
+            </Row>
+            <Button type="submit" value="submit" color="primary">Submit</Button>
+            </LocalForm>
+            
+        </ModalBody>
+           </Modal>
+          
+        </div>
+        )
+        
+    }
+  
+}
   function RenderDish({dish}) {
         if(dish != null) {
             return (
@@ -36,10 +115,14 @@ import {Link} from 'react-router-dom';
                                 <div>{comment.comment}</div>
                     <div>--- {comment.author} ,{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</div>
                             </li>
+                            
                         </ul>
+                     
                     )
-                })
-               
+                    })
+                
+
+
             )
         }
         else{
@@ -77,12 +160,15 @@ import {Link} from 'react-router-dom';
                         <div className='col-12 col-md-5 m-1'>
                         <h4>Comments</h4>
                             <RenderComments comments = {props.comments}/>
+                            <CommentForm/>
                        </div>
                     </div>
                 </div>
             )
         }
     }
+
+ 
 
 
 export default DishDetail;
